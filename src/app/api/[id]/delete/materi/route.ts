6ff2acf6d10fbeb5100cc22578eee78018686cf5
bagasrr <bagasramadhan239@gmail.com>
2025-07-03 19:26:58 +0700

    ubah jadi patch

--- src/app/api/[id]/delete/materi/route.ts
+++ src/app/api/[id]/delete/materi/route.ts
@@ -1,14 +1,17 @@
 // contoh untuk /api/[id]/delete/materi/route.ts
+import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
   const { id } = params;
+  const session = await getServerSession(authOptions);
 
   try {
     await prisma.materi.update({
       where: { id },
-      data: { IsDeleted: new Date() },
+      data: { IsDeleted: new Date(), LastUpdateDate: new Date(), LastUpdatedBy: session?.user.email || "system" },
     });
 
     return NextResponse.json({ message: "materi berhasil di-soft delete" });
